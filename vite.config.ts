a6bde3d5d8cea02e275ca73065dadf47049e29df
alexstudentCoder14 <alexandre.chassagne@cathstu.tas.edu.au>
2026-09-21 17:02:01 +1000

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,16 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/online-game-maker/',
   plugins: [react()],
   build: {
     chunkSizeWarningLimit: 900,
     rollupOptions: {
-      output: { manualChunks: { three: ['three'] } },
+      output: {
+        manualChunks: {
+          three: ['three'],
+        },
+      },
     },
   },
 })
